713f63e7368505495836830da64dc15ac41c959a
Brenda Duarte <160661067+Brend4wh@users.noreply.github.com>
2024-11-17 12:44:40 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("cadastrarPaciente").addEventListener("click", function() {
+    window.location.href = "cadastroPaciente.html";
+});
+
+document.getElementById("logarPaciente").addEventListener("click", function() {
+    window.location.href = "loginPaciente.html";
+});
+
+document.getElementById("cadastrarMedico").addEventListener("click", function() {
+    window.location.href = "cadastroMedico.html";
+});
+
+document.getElementById("logarMedico").addEventListener("click", function() {
+    window.location.href = "loginMedico.html";
+});
